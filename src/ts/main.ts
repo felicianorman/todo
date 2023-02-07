44b8fdb4d7e70f969148b96570443f484dcf4600
Felicia <87656480+felicianorman@users.noreply.github.com>
2023-02-07 21:06:14 +0100

    fixade bugg och la till ikon

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,37 +3,53 @@ import { todos } from "./models/todo";
 let wrapper = document.getElementById("todo") as HTMLDivElement;
 let todoInput = document.getElementById("addTodo") as HTMLInputElement;
 let addBtn = document.getElementById("todoBtn") as HTMLButtonElement;
+let ul = document.createElement("ul") as HTMLUListElement;
+
+let todo: todos[] = JSON.parse(localStorage.getItem("todos") || "[]");
 
 function newTodo() {
-  let todo: todos[] = [];
 
   addBtn.addEventListener("click", () => {
+  
     let newTodo = new todos(todoInput.value, false);
     todo.push(newTodo);
+
     localStorage.setItem("todos", JSON.stringify(todo));
+    ul.innerHTML = "";
+
     createHTML();
+
     todoInput.value = " ";
   });
+ 
 }
 
 function createHTML() {
   let getTodo: any = localStorage.getItem("todos");
   let todos = JSON.parse(getTodo);
+  console.log(todos);
 
+  wrapper.appendChild(ul);
   for (let i = 0; i < todos.length; i++) {
-    console.log(todos[i]);
-
-    let ul = document.createElement("ul") as HTMLUListElement;
     ul.classList.add("list");
     let li = document.createElement("li") as HTMLLIElement;
     li.classList.add("list--item");
 
-    li.innerHTML = todos[i].todo;
-
+    li.innerHTML =
+      todos[i].todo + `<span class="fa-solid fa-trash" id="icon"></span>`;
     ul.appendChild(li);
-    wrapper.appendChild(ul);
   }
+}
 
+function deleteTodo() {
+  let trashCan = document.querySelectorAll(".fa-trash");
+  trashCan.forEach((remove) => {
+    remove.addEventListener("click", () => {
+      console.log("hejdå");
+    });
+  });
 }
 
 newTodo();
+createHTML();
+deleteTodo();
